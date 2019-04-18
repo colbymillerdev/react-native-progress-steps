2ff78f11e7d62d55e19bc8ec4849ac911e8e8223
skiddolo <skiddolo@gmail.com>
2019-04-18 15:11:01 +0200

    Update ProgressSteps.js
    added custom container style

--- src/ProgressSteps/ProgressSteps.js
+++ src/ProgressSteps/ProgressSteps.js
@@ -61,7 +61,7 @@ class ProgressSteps extends Component {
     };
 
     return (
-      <View style={{ flex: 1 }}>
+      <View style={[{ flex: 1 }, this.props.customContainerStyle]}>
         <View style={styles.stepIcons}>{this.renderStepIcons()}</View>
         <View style={{ flex: 1 }}>
           {React.cloneElement(this.props.children[this.state.activeStep], {
