45cceea41053550e369ff394fa9c4ab80218550c
ninadvartak1989 <62302837+ninadvartak1989@users.noreply.github.com>
2020-03-17 13:25:55 -0400

    Update progress button style

--- src/ProgressSteps/ProgressButtons.js
+++ src/ProgressSteps/ProgressButtons.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { View } from 'react-native';
 
 const ProgressButtons = props => (
-  <View style={{ flexDirection: 'row', marginTop: 90 }}>
-    <View style={{ position: 'absolute', left: 60, bottom: 40 }}>{props.renderPreviousButton()}</View>
-    <View style={{ position: 'absolute', right: 60, bottom: 40 }}>{props.renderNextButton()}</View>
+  <View style={{ flexDirection: 'row', marginTop: 40 }}>
+    <View style={{ position: 'absolute', left: 60, bottom: 10 }}>{props.renderPreviousButton()}</View>
+    <View style={{ position: 'absolute', right: 60, bottom: 10 }}>{props.renderNextButton()}</View>
   </View>
 );
 
