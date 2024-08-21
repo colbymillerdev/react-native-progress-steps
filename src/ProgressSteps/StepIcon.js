370543adaafa6d3edc02758395fbbd182f397125
Muhammad-Farhan <73163937+farhan26azam@users.noreply.github.com>
2024-08-21 16:07:30 +0500

    numbers centered

--- src/ProgressSteps/StepIcon.js
+++ src/ProgressSteps/StepIcon.js
@@ -9,6 +9,7 @@ class StepIcon extends Component {
     if (this.props.isActiveStep) {
       styles = {
         circleStyle: {
+          justifyContent: 'center',
           width: 40,
           height: 40,
           borderRadius: 20,
@@ -19,7 +20,6 @@ class StepIcon extends Component {
         },
         circleText: {
           alignSelf: 'center',
-          top: 20 / 3,
         },
         labelText: {
           textAlign: 'center',
@@ -57,6 +57,7 @@ class StepIcon extends Component {
     } else if (this.props.isCompletedStep) {
       styles = {
         circleStyle: {
+          justifyContent: 'center';
           width: 36,
           height: 36,
           borderRadius: 18,
@@ -64,7 +65,6 @@ class StepIcon extends Component {
         },
         circleText: {
           alignSelf: 'center',
-          top: 18 / 2,
         },
         labelText: {
           textAlign: 'center',
